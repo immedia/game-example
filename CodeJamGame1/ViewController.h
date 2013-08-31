5531d770aee584edd21de37b65ef3d10bba354ec
Jitesh Nunnan <jiteshnunnan@gmail.com>
2013-08-31 14:41:24 +0200

    GM

--- CodeJamGame1/ViewController.h
+++ CodeJamGame1/ViewController.h
@@ -10,4 +10,12 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+@property (weak, nonatomic) IBOutlet UIButton *goButton;
+@property (weak, nonatomic) IBOutlet UITextField *answerTextField;
+
+- (IBAction)goButtonPressed:(id)sender;
+
 @end
--- CodeJamGame1/ViewController.m
+++ CodeJamGame1/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, retain) NSString *answer;
+@property NSInteger score, timeRemaining;
+
 @end
 
 @implementation ViewController
@@ -18,12 +21,51 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  self.score = 0;
+  self.timeRemaining = 20;
+  self.scoreLabel.text = [NSString stringWithFormat:@"%i", self.score];
+  self.timeLabel.text = [NSString stringWithFormat:@"%i", self.timeRemaining];
+  
+  [self performSelector:@selector(changeTime) withObject:self afterDelay:1.0];  
+  [self changeQuestionLabel];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)goButtonPressed:(id)sender {
+  if ([self.answerTextField.text isEqualToString:self.answer]) {
+    self.view.backgroundColor = [UIColor greenColor];
+    self.score = self.score + 10;
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i", self.score];
+  } else {
+    self.view.backgroundColor = [UIColor redColor];
+  }
+  self.answerTextField.text = @"";
+  [self changeQuestionLabel];  
+}
+
+- (void)changeQuestionLabel {
+  int firstNumber = arc4random() % 100;
+  int secondNumber = arc4random() % 100;
+  self.answer = [NSString stringWithFormat:@"%i", firstNumber + secondNumber];
+  NSLog(@"answer %@", self.answer);
+  self.questionLabel.text = [NSString stringWithFormat:@"%i + %i", firstNumber, secondNumber];
+}
+
+- (void)changeTime {
+  self.timeRemaining = self.timeRemaining - 1;
+  self.timeLabel.text = [NSString stringWithFormat:@"%i", self.timeRemaining];
+  if (self.timeRemaining > 0) {
+    [self performSelector:@selector(changeTime) withObject:self afterDelay:1.0];
+  } else {
+    [self.goButton setEnabled:NO];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"CodeJam" message:[NSString stringWithFormat:@"Your score is %i", self.score] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil,  nil];
+    [alert show];
+  }
 }
 
 @end
